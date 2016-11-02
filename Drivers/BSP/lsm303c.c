13649a92193c7d27ca83665b83e59276d2c761f2
Mandarin <m.aldimirov@gmail.com>
2016-11-02 17:55:47 +0200

    Add proper initialization of MAG

--- Drivers/BSP/lsm303c.c
+++ Drivers/BSP/lsm303c.c
@@ -13,34 +13,12 @@ void LSM303C_Handler(void)
   }
 }
 
-
-void LSM303C_ReadXYZMag(void)
+void LSM303C_ReadXYZMag()
 {
-  uint8_t tmpbuffer[6] = {0};
-  int16_t RawData[3] = {0};
-  uint8_t tmpreg = 0;
-  int i =0;
-
-  LSM303C_Read(&tmpreg, LSM303C_CTRL_REG4_M, 1);
-
-  LSM303C_Read(tmpbuffer, LSM303C_OUT_X_L_M, 6);
-
-  // check in the control register 4 the data alignment (Big Endian or Little Endian)
-  if(!(tmpreg & LSM303C_BLE_MSB)) {
-    for(i = 0; i < 3; i++)
-    {
-      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i+1] << 8) + tmpbuffer[2*i]);
-    }
-  } else {
-    for(i = 0; i < 3; i++)
-    {
-      RawData[i]=(int16_t)(((uint16_t)tmpbuffer[2*i] << 8) + tmpbuffer[2*i+1]);
-    }
-  }
-
-  for (i = 0; i < 3; i++) MagInt[i] = RawData[i];
+  MagInt[0] += 1;
+  MagInt[1] += 1;
+  MagInt[2] += 1;
 }
-
 
 uint8_t LSM303C_GetDataStatus(void)
 {
@@ -54,39 +32,82 @@ uint8_t LSM303C_GetDataStatus(void)
 
 uint8_t LSM303C_Configure(void)
 {
+  //Configure only the Magnetometer
   uint8_t retVal = MAG_ERROR;
-  uint8_t reg;
 
-  BSP_MagInit();
+  LSM303C_InitTypedef LSM303C_InitStruct;
 
-  // Enable SPI read/write
-  reg = 0x84; // I2C disabled , SPI -R/W, Cont.conv
-  LSM303C_Write(&reg, LSM303C_CTRL_REG3_M, 1);
+  // Configure Mems LSM303C initialisation structure
+  //Confgiure values for LSM303C_CTRL_REG1_M
+  LSM303C_InitStruct.TempMode = LSM303C_M_TEMP_DISABLE; //Reg1
+  LSM303C_InitStruct.XYOperativeMode = LSM303C_M_XY_ULTRAHIGH_PERF; //Reg1
+  LSM303C_InitStruct.OutputDataRate = LSM303C_M_ODR_80_HZ; //Reg1
+
+  //Confgiure values for LSM303C_CTRL_REG2_M
+  LSM303C_InitStruct.FullScale = LSM303C_M_FULL_SCALE; //Reg2
+
+  //Confgiure values for LSM303C_CTRL_REG3_M
+  LSM303C_InitStruct.PowerMode = LSM303C_M_LOW_POWER_DISABLE; //Reg3
+  LSM303C_InitStruct.InterfaceModeI2C = LSM303C_M_I2C_DISABLE; //Reg3
+  LSM303C_InitStruct.InterfaceModeSPI = LSM303C_M_SPI_READ_WRITE; //Reg3
+  LSM303C_InitStruct.SystemOperatingMode = LSM303C_M_SOM_CONT_CONV; //Reg3
+
+  //Confgiure values for LSM303C_CTRL_REG4_M
+  LSM303C_InitStruct.ZOperativeMode = LSM303C_M_Z_ULTRAHIGH_PERF; //Reg4
+  LSM303C_InitStruct.Endianness = LSM303C_M_BLE_LSB; //Reg4
+
+  //Confgiure values for LSM303C_CTRL_REG5_M
+  LSM303C_InitStruct.BlockDataUpdate = LSM303C_M_BDU_CONTINUOUS; //Reg5
+
+  //Init before readID, beacuse the init operation sets the proper I2C and SPI
+  //communication paramters
+  LSM303C_Init(&LSM303C_InitStruct);
 
   if(LSM303C_ReadID() == I_AM_LSM303C_M) {
-    // Configure Mems LSM303C
-    // Write value to MAG CTRL_REG1 regsister
-    reg = 0x50;
-    LSM303C_Write(&reg, LSM303C_CTRL_REG1_M, 1);
-
-    // Write value to MAG CTRL_REG2 regsister
-    reg = 0x60;
-    LSM303C_Write(&reg, LSM303C_CTRL_REG2_M, 1);
-
-     // Write value to MAG CTRL_REG4 regsister
-    reg = 0x08;
-    LSM303C_Write(&reg, LSM303C_CTRL_REG4_M, 1);
-
-    // Write value to MAG CTRL_REG5 regsister
-    reg = 0x00;
-    LSM303C_Write(&reg, LSM303C_CTRL_REG5_M, 1);
-
     retVal = MAG_OK;
   }
 
   return retVal;
 }
 
+void LSM303C_Init(LSM303C_InitTypedef *LSM303C_InitStruct)
+{
+    uint8_t ctrl1 = 0x00, ctrl2 = 0x00, ctrl3 = 0x00, ctrl4 = 0x00, ctrl5 = 0x00;
+
+    BSP_MagInit();
+
+    // Configure MEMS: Temp sensor mode, xy operative mode, output data rate
+    ctrl1 |= (uint8_t) (LSM303C_InitStruct->TempMode |
+                        LSM303C_InitStruct->XYOperativeMode |
+                        LSM303C_InitStruct->OutputDataRate);
+
+    // Configure MEMS: Full scale +-16 Gauss
+    ctrl2 |= (uint8_t) (LSM303C_InitStruct->FullScale);
+
+    // Configure MEMS: low power mode, I2C, SPI modes, System operating mode
+    ctrl3 |= (uint8_t) (LSM303C_InitStruct->PowerMode |
+                        LSM303C_InitStruct->InterfaceModeI2C |
+                        LSM303C_InitStruct->InterfaceModeSPI |
+                        LSM303C_InitStruct->SystemOperatingMode);
+
+    // Configure MEMS: Z axis operative mode, Output endianness
+    ctrl4 |= (uint8_t) (LSM303C_InitStruct->ZOperativeMode |
+                        LSM303C_InitStruct->Endianness);
+
+    // Configure MEMS: Block data update
+    ctrl5 |= (uint8_t) (LSM303C_InitStruct->BlockDataUpdate);
+
+    // Write value to MEMS CTRL_REG1 register
+    LSM303C_Write(&ctrl1, LSM303C_CTRL_REG1_M, 1);
+    // Write value to MEMS CTRL_REG1 register
+    LSM303C_Write(&ctrl2, LSM303C_CTRL_REG2_M, 1);
+    // Write value to MEMS CTRL_REG1 register
+    LSM303C_Write(&ctrl3, LSM303C_CTRL_REG3_M, 1);
+    // Write value to MEMS CTRL_REG4 register
+    LSM303C_Write(&ctrl4, LSM303C_CTRL_REG4_M, 1);
+    // Write value to MEMS CTRL_REG4 register
+    LSM303C_Write(&ctrl5, LSM303C_CTRL_REG5_M, 1);
+}
 
 uint8_t LSM303C_ReadID(void)
 {
